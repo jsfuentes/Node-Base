d4a7aa9a3c7dbf53be54ed77f5e38d5847f3c03d
Jorge <jjfuentes2020@gmail.com>
2020-11-07 19:36:00 -0700

    Update packages and add google login route

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const conf = require("config");
+const axios = require("axios");
 const debug = require("debug")("app:routes:users");
 
 const User = require("../models/User.js");
@@ -8,6 +10,7 @@ let router = express.Router();
 
 router.get("/", asyncH(getUser));
 router.get("/add", asyncH(addUser));
+router.get("/success", asyncH(afterGoogleLogin));
 
 //Returns all users rn
 async function getUser(req, res) {
@@ -19,7 +22,7 @@ async function getUser(req, res) {
 const sampleTest = {
   test: true,
   name: "Jorge Fuentes",
-  email: "email@testing.com"
+  email: "email@testing.com",
 };
 
 //For easy testing is get
@@ -27,6 +30,74 @@ async function addUser(req, res) {
   const newUser = new User(sampleTest);
   const resp = await newUser.save();
   res.json(resp);
+}
+
+//login from website after google redirect
+async function afterGoogleLogin(req, res) {
+  if (!req.session.grant || !req.session.grant.response) {
+    console.error(`Login success failed, grant: ${req.session.grant}`);
+    res.redirect("/");
+    return;
+  }
+
+  const { access_token, refresh_token } = req.session.grant.response;
+  const user = await getOrCreateGoogleUser(access_token);
+  req.session.uid = user._id;
+
+  res.redirect(conf.get("client_url"));
+}
+
+async function getOrCreateGoogleUser(gaccess_token) {
+  let profile;
+  resp = await axios.get(
+    `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${gaccess_token}`
+  );
+  profile = resp.data;
+
+  const payload = {
+    gid: profile.id,
+    gaccess_token,
+    verified_email: profile.verified_email,
+    name: profile.name,
+    pic: profile.picture,
+    email: profile.email,
+    locale: profile.locale,
+    status: "ready",
+  };
+
+  const newUser = await getOrCreateUser(payload);
+  return newUser;
+}
+
+//Get user or check the company
+async function getOrCreateUser(payload) {
+  let user;
+  user = await User.findOne({ email: payload.email }).exec();
+  if (user && user.status === "ready") {
+    //Old User
+    let newTokens = {};
+    //only override tokens in database
+    if (payload.gaccess_token) {
+      newTokens.gaccess_token = payload.gaccess_token;
+    }
+
+    user = await User.findOneAndUpdate({ email: payload.email }, newTokens, {
+      new: true,
+    }).exec();
+    debug("Getting oldUser", user);
+  } else if (user && user.status !== "ready") {
+    //Update User
+    user = await User.findOneAndUpdate({ _id: user._id }, payload, {
+      new: true,
+    }).exec();
+    debug("Updated user", user);
+  } else {
+    const userModel = new User(payload);
+    user = await userModel.save();
+    debug("Created newUser", user);
+  }
+
+  return user;
 }
 
 module.exports = router;
